45f2b25baad1cf3f9ea4646710010a72a059e70b
lmh <lmh@weiyian.com>
2020-12-03 14:50:56 +0800

    init

--- src/Request/PaymentRefundCreateRequest.php
+++ src/Request/PaymentRefundCreateRequest.php
@@ -12,5 +12,28 @@ namespace Lmh\OpenPaySDK\Request;
 
 class PaymentRefundCreateRequest extends BaseRequest
 {
-
+    /**
+     * @var string 商户订单号
+     */
+    public $outTradeNo;
+    /**
+     * @var string 交易流水号
+     */
+    public $tradeNo;
+    /**
+     * @var string 商户退款单号可以为空
+     */
+    public $outRefundNo;
+    /**
+     * @var int 需要退款的金额，该金额不能大于订单金额
+     */
+    public $refundAmount;
+    /**
+     * @var string 退款的原因说明
+     */
+    public $refundReason;
+    /**
+     * @var string 通知商户服务器地址
+     */
+    public $notifyUrl;
 }
