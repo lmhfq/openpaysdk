299de144e5d4b69ef9cf3ff9cb3924c735ab7608
lmh <lmh@weiyian.com>
2020-12-04 17:16:27 +0800

    init

--- tests/DefaultTest.php
+++ tests/DefaultTest.php
@@ -5,12 +5,7 @@ namespace Lmh\OpenPaySDK\Tests;
 
 use Lmh\OpenPaySDK\Client;
 use Lmh\OpenPaySDK\Constant\TradeType;
-use Lmh\OpenPaySDK\Request\MerchantChannelCreateRequest;
-use Lmh\OpenPaySDK\Request\MerchantChannelListRequest;
-use Lmh\OpenPaySDK\Request\MerchantCreateRequest;
-use Lmh\OpenPaySDK\Request\PaymentTradeQueryRequest;
 use Lmh\OpenPaySDK\Request\PaymentTradeUnifiedOrderRequest;
-use Lmh\OpenPaySDK\Response\MerchantCreateResponse;
 use Lmh\OpenPaySDK\Response\PaymentTradeUnifiedOrderResponse;
 use PHPUnit\Framework\TestCase;
 
@@ -37,35 +32,7 @@ class DefaultTest extends TestCase
         $paymentProductListRequest->totalAmount = 100;
         $paymentProductListRequest->openid = 'oUpF8uMuAJO_M2pxb1Q9zNjWeS6o';
 
-//
-//        $paymentProductListRequest = new PaymentTradeQueryRequest();
-//        $paymentProductListRequest->externalId = "1698";
-//        $paymentProductListRequest->outTradeNo = '1606964752';
-//
-//
-//
-//        $paymentProductListRequest = new MerchantCreateRequest();
-//        $paymentProductListRequest->externalId = "1698";
-//        $paymentProductListRequest->merchantNature = 2;
-//        $paymentProductListRequest->merchantName = "1698直连测试1";
-//
-//
-//
-//        $paymentProductListRequest = new MerchantChannelListRequest();
-//        $paymentProductListRequest->externalId = "1698";
-//
-//
-//        $paymentProductListRequest = new MerchantChannelCreateRequest();
-//        $paymentProductListRequest->externalId = "1698";
-//        $paymentProductListRequest->subAppId ='wx06b20e93d5f2a10d';
-//        $paymentProductListRequest->subMchId ='1333182201';
-//        $paymentProductListRequest->key ='zhejiangweiyianxinlingshouceshib';
-//        $paymentProductListRequest->channelCode ='WECHAT';
-
-
-//        $paymentProductListRequest = new MerchantChannelListRequest();
-//        $paymentProductListRequest->externalId = "1698";
-        $result = $client->execute($paymentProductListRequest,new PaymentTradeUnifiedOrderResponse());
+        $result = $client->execute($paymentProductListRequest, new PaymentTradeUnifiedOrderResponse());
 
     }
 }
