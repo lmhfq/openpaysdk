a905ad9d85131f2882bcc7b6a3502579b0311675
lmh <lmh@weiyian.com>
2020-12-29 11:12:55 +0800

    企业付款到零钱

---
+++ src/Request/WechatTransferBalanceRequest.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: lmh <lmh@weiyian.com>
+ * Date: 2020/12/29
+ * Time: 上午11:09
+ */
+
+namespace Lmh\OpenPaySDK\Request;
+
+
+class WechatTransferBalanceRequest extends BaseRequest
+{
+    /**
+     * @var string 商户订单号
+     */
+    public $outTradeNo;
+    /**
+     * @var string
+     */
+    public $openid;
+    /**
+     * @var string 校验用户姓名选项 NO_CHECK：不校验真实姓名, FORCE_CHECK：强校验真实姓名
+     */
+    public $checkName = 'NO_CHECK';
+    /**
+     * @var string 收款用户姓名
+     */
+    public $reUserName;
+    /**
+     * @var int
+     */
+    public $totalAmount;
+    /**
+     * @var string 企业付款备注
+     */
+    public $description;
+    /**
+     * @var int
+     */
+    public $limit;
+}
---
+++ src/Response/WechatTransferBalanceResponse.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: lmh <lmh@weiyian.com>
+ * Date: 2020/12/29
+ * Time: 上午11:09
+ */
+
+namespace Lmh\OpenPaySDK\Response;
+
+
+class WechatTransferBalanceResponse extends BaseResponse
+{
+
+}
