ad83c2f74fd946b7ecbe622d75108dfcac3b7bd9
lmh <lmh@weiyian.com>
2021-01-26 16:52:54 +0800

    字段调整

--- src/Request/MerchantChannelCreateRequest.php
+++ src/Request/MerchantChannelCreateRequest.php
@@ -28,15 +28,15 @@ class MerchantChannelCreateRequest extends BaseRequest
     /**
      * @var string
      */
-    public $publicKey;
+    public $aesKey;
+    /**
+     * @var string
+     */
+    public $serialNumber;
     /**
      * @var string
      */
     public $certPath;
-    /**
-     * @var string
-     */
-    public $keyPath;
     /**
      * @var int
      */
@@ -51,17 +51,6 @@ class MerchantChannelCreateRequest extends BaseRequest
     public $status;
     /**
      * @var string
-     * 1    ALIPAY    支付宝支付
-     * 2    WECHAT    微信支付
-     * 4    CPCN_BALANCE    钱包支付(中金)
-     * 5    UNIONPAY    银联支付
-     * 6    WECHAT_MICRO    微信小程序
-     * 7    WECHAT_SP    微信支付(服务商)
-     * 8    CPCN_ALIPAY    支付宝支付(中金)
-     * 9    CPCN_WECHAT    微信支付(中金)
-     * 10    WECHAT_APP    微信APP
-     * 11    WECHAT_SP_MICRO    微信小程序(服务商)
-     * 12    WECHAT_SP_APP    微信APP(服务商)
      */
     public $channelCode;
     /**
