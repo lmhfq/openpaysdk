403ab181e37163f2296de6b3f4e9d51a93b70909
lmh <lmh@weiyian.com>
2021-11-16 14:31:43 +0800

    新增接口

--- src/Request/AccountDeleteRequest.php
+++ src/Request/AccountDeleteRequest.php
@@ -6,7 +6,7 @@ namespace Lmh\OpenPaySDK\Request;
 class AccountDeleteRequest extends BaseRequest
 {
     protected $method = 'account.delete';
-    /**AccountCancelRequest
+    /**
      * @var int
      */
     public $accountId;
