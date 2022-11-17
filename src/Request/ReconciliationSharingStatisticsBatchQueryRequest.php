f3977ccc9ba7a6ae112d172caf7a47a4d1333fe6
lmh <lmh@weiyian.com>
2022-11-17 09:54:02 +0800

    升级新接口

--- src/Request/ReconciliationSharingStatisticsBatchQueryRequest.php
+++ src/Request/ReconciliationSharingStatisticsBatchQueryRequest.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 
 namespace Lmh\OpenPaySDK\Request;
 
-class ReconciliationSharingBatchQueryRequest extends BaseRequest
+class ReconciliationSharingStatisticsBatchQueryRequest extends BaseRequest
 {
-    protected $method = 'reconciliation.sharing.batch.query';
+    protected $method = 'reconciliation.sharing.statistics.batch.query';
     /**
      * @var int 分账状态 1-待分账  2-已分账 3-冻结在总部
      */
--- src/Response/ReconciliationSharingBatchQueryResponse.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Lmh\OpenPaySDK\Response;
-
-class ReconciliationSharingBatchQueryResponse extends BaseResponse
-{
-}
